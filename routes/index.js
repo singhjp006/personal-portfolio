547265b7ad9064df4a92798b29796fa7eedc9940
singhjp006 <jashanpreet@khalsa.com>
2021-02-14 15:41:36 +0530

    contact page and added content

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/services', function(req, res, next) {
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact-me', { title: 'Contact Me' });
 });
 
 
